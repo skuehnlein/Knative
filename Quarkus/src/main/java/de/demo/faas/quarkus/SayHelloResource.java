9bf30d1e43f15dbd17ce3551920f6d46b01ef947
Stefan Kühnlein <stefan.kuehnlein@opitz-consulting.com>
2019-07-18 07:50:04 +0200

    initiale Erstellung

---
+++ Quarkus/src/main/java/de/demo/faas/quarkus/SayHelloResource.java
@@ -0,0 +1,37 @@
+package de.demo.faas.quarkus;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Path("/")
+public class SayHelloResource {
+
+	@Inject
+	SayHelloService greetingService;
+	
+	@ConfigProperty(name = "service.hostname")
+	String hostName;
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/")
+	public String hello() {
+		return greetingService.sayHello("Hello: This is the resonsponse of: " + hostName);
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/")
+	public String eventHello(String pKnativeEvent) {
+		return greetingService.eventSayHello(pKnativeEvent);
+	}
+
+}
---
+++ Quarkus/src/main/java/de/demo/faas/quarkus/SayHelloService.java
@@ -0,0 +1,47 @@
+package de.demo.faas.quarkus;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.vertx.core.json.JsonObject;
+
+@ApplicationScoped
+public class SayHelloService {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SayHelloService.class);
+
+	private int count = 0;
+
+	private final SimpleDateFormat SDF = new SimpleDateFormat("HH:mm:ss");
+
+	private static final String RESPONSE_STRING_FORMAT = "%s hello => '%s' : %d\n";
+
+	private static final String HOSTNAME = parseContainerIdFromHostname(
+			System.getenv().getOrDefault("HOSTNAME", "unknown"));
+
+	static String parseContainerIdFromHostname(String hostname) {
+		return hostname.replaceAll("hello-v\\d+-", "").trim();
+	}
+
+	public String sayHello(String prefix) {
+		count++;
+		return String.format(RESPONSE_STRING_FORMAT, prefix, HOSTNAME, count);
+	}
+
+	public String eventSayHello(String pKnativeEvent) {
+		count++;
+	    String hostname = String.format(RESPONSE_STRING_FORMAT, ""," Event ", HOSTNAME, count);
+	    JsonObject response = new JsonObject(pKnativeEvent)
+	                            .put("host", hostname.replace("\n", "").trim())
+	                            .put("time", SDF.format(new Date()));
+	    LOGGER.info("Event Message Received \n {}", response.encodePrettily());
+	    return response.encode();
+	}
+
+
+}
